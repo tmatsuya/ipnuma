542e3e9b77659a0ef2f6634cd42bbf3052d98496
Takeshi MATSUYA <macchan@sfc.wide.ad.jp>
2013-07-15 02:58:27 +0900

    udpclient.c: support memory write command

--- boards/ecp3versa/test/udpclient.c
+++ boards/ecp3versa/test/udpclient.c
@@ -11,7 +11,8 @@
 #define		SERV_PORT	3422		// USB over IP
 #define		BUFSIZE		1024
 
-// sudo arp -s 10.0.21.99 00:37:65:00:00:01
+// sudo arp -s 10.0.21.99 00:37:76:00:00:01
+// sudo arp -s 203.178.139.139 00:37:76:00:00:01
 
 int main(int argc, char **argv)
 {
@@ -19,13 +20,24 @@ int main(int argc, char **argv)
 	struct sockaddr_in servaddr;
 	int i, j, dwlen, dwlen2;
 	char sdata[BUFSIZE], dest_ip[256];
+	long int write_addr, write_data;
 
-	if ( argc != 2) {
-		fprintf( stderr, "usage: %s [dest IP]\n", argv[0] );
+	if ( argc < 2) {
+		fprintf( stderr, "usage: %s [dest IP] <write_address> <write_data>\n", argv[0] );
 		exit(-1);
 	}
 
 	strcpy( dest_ip, argv[1] );
+
+	if ( argc >= 3 ) {
+		write_addr = strtol( argv[2], NULL, 0);
+		printf("write_addr=%08x\n", write_addr);
+	}
+
+	if ( argc >= 4 ) {
+		write_data = strtol( argv[3], NULL, 0);
+		printf("write_data=%08x\n", write_data);
+	}
 
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
@@ -33,7 +45,7 @@ int main(int argc, char **argv)
 	inet_aton(dest_ip, &servaddr.sin_addr);
 
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if(sockfd < 0){
+	if (sockfd < 0){
 		perror("socket()");
 		exit(EXIT_FAILURE);
 	}
@@ -47,39 +59,59 @@ int main(int argc, char **argv)
 	sdata[ 2] = 0x00;
 	sdata[ 3] = 0x00;
 
-	sdata[ 4] = 0x80|dwlen;// write command(b7), 32bit(b6), length(b5-b0)=4DW
-	sdata[ 5] = 0xff;	// LBE(b8-4), FBE(b3-0)
+	if ( argc == 2 ) {
 
-	sdata[ 6] = 0xd0;	// ADDR=0xD0000000
-	sdata[ 7] = 0x00;
-	sdata[ 8] = 0x00;
-	sdata[ 9] = 0x00;
+		sdata[ 4] = 0x80|dwlen;// write command(b7), 32bit(b6), length(b5-b0)=4DW
+		sdata[ 5] = 0xff;	// LBE(b8-4), FBE(b3-0)
 
-	sdata[dwlen*4+10] = 0x80|dwlen;// write command(b7), 32bit(b6), length(b5-b0)=4DW
-	sdata[dwlen*4+11] = 0xff;	// LBE(b8-4), FBE(b3-0)
+		sdata[ 6] = 0xc0;	// ADDR=0xD0000000
+		sdata[ 7] = 0x00;
+		sdata[ 8] = 0x00;
+		sdata[ 9] = 0x00;
 
-	sdata[dwlen*4+12] = 0xd0;	// ADDR=0xD0000000
-	sdata[dwlen*4+13] = 0x00;
-	sdata[dwlen*4+14] = 0x02;
-	sdata[dwlen*4+15] = 0x80;
+		sdata[dwlen*4+10] = 0x80|dwlen;// write command(b7), 32bit(b6), length(b5-b0)=4DW
+		sdata[dwlen*4+11] = 0xff;	// LBE(b8-4), FBE(b3-0)
 
+		sdata[dwlen*4+12] = 0xc0;	// ADDR=0xD0000000
+		sdata[dwlen*4+13] = 0x00;
+		sdata[dwlen*4+14] = 0x02;
+		sdata[dwlen*4+15] = 0x80;
 
-	for ( j=1 ; ; ++j) {
-		for (i=0; i<dwlen; ++i) {
-			sdata[ 10+i*4+0 ] = ((i+j)&0xf)*0x10+0;
-			sdata[ 10+i*4+1 ] = ((i+j)&0xf)*0x10+1;
-			sdata[ 10+i*4+2 ] = ((i+j)&0xf)*0x10+2;
-			sdata[ 10+i*4+3 ] = ((i+j)&0xf)*0x10+3;
-			sdata[ 16+(i+dwlen)*4+0 ] = ((i+j+1)&0xf)*0x10+0;
-			sdata[ 16+(i+dwlen)*4+1 ] = ((i+j+1)&0xf)*0x10+1;
-			sdata[ 16+(i+dwlen)*4+2 ] = ((i+j+1)&0xf)*0x10+2;
-			sdata[ 16+(i+dwlen)*4+3 ] = ((i+j+1)&0xf)*0x10+3;
+		for ( j=1 ; ; ++j) {
+			for (i=0; i<dwlen; ++i) {
+				sdata[ 10+i*4+0 ] = ((i+j)&0xf)*0x10+0;
+				sdata[ 10+i*4+1 ] = ((i+j)&0xf)*0x10+1;
+				sdata[ 10+i*4+2 ] = ((i+j)&0xf)*0x10+2;
+				sdata[ 10+i*4+3 ] = ((i+j)&0xf)*0x10+3;
+				sdata[ 16+(i+dwlen)*4+0 ] = ((i+j+1)&0xf)*0x10+0;
+				sdata[ 16+(i+dwlen)*4+1 ] = ((i+j+1)&0xf)*0x10+1;
+				sdata[ 16+(i+dwlen)*4+2 ] = ((i+j+1)&0xf)*0x10+2;
+				sdata[ 16+(i+dwlen)*4+3 ] = ((i+j+1)&0xf)*0x10+3;
+			}
+//write	(1,sdata,18+(4*dwlen*2)+1);
+			if (sendto(sockfd, sdata, 16+(4*dwlen*2)+1, 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
+				perror("sendto()");
+			}
+			usleep(1);
 		}
-//write(1,sdata,18+(4*dwlen*2)+1);
-		if (sendto(sockfd, sdata, 16+(4*dwlen*2)+1, 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
+	} else {
+		sdata[ 4] = 0x81;// write command(b7), 32bit(b6), length(b5-b0)=4DW
+		sdata[ 5] = 0xff;	// LBE(b8-4), FBE(b3-0)
+
+		sdata[ 6] = (write_addr >> 24) & 0xff;	// ADDR=0xD0000000
+		sdata[ 7] = (write_addr >> 16) & 0xff;
+		sdata[ 8] = (write_addr >>  8) & 0xff;
+		sdata[ 9] = (write_addr >>  0) & 0xff;
+
+		sdata[10] = (write_data >>  0) & 0xff;
+		sdata[11] = (write_data >>  8) & 0xff;
+		sdata[12] = (write_data >> 16) & 0xff;
+		sdata[13] = (write_data >> 24) & 0xff;
+
+		if (sendto(sockfd, sdata, 10+(4*dwlen)+1, 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
 			perror("sendto()");
 		}
-		usleep(1);
+
 	}
 
 	exit(EXIT_SUCCESS);
