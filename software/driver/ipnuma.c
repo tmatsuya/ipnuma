719e917c90df2231f51841a1ff11a1352b695a85
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-07-19 14:10:13 +0900

    driver name changed to ipnuma

--- software/driver/ipnuma.c
+++ software/driver/ipnuma.c
@@ -10,21 +10,12 @@
 #include <linux/version.h>
 
 #ifndef DRV_NAME
-#define DRV_NAME        "pcidma"
-#endif
-#ifndef RX_ADDR
-#define RX_ADDR         0x8000
-#endif
-#ifndef TX_ADDR
-#define TX_ADDR         0x9000
+#define DRV_NAME	"ipnuma"
 #endif
 
-#define	DRV_VERSION	"0.0.2"
-#define	pcidma_DRIVER_NAME	DRV_NAME " PCIDMA driver " DRV_VERSION
-#define	PACKET_BUF_MAX	(1024*1024)
-#define	TEMP_BUF_MAX	2000
-
-#define HEADER_LEN	16
+#define	DRV_VERSION	"0.0.1"
+#define	ipnuma_DRIVER_NAME	DRV_NAME " IP-NUMA driver " DRV_VERSION
+#define	DMA_BUF_SIZE	(1024*1024)
 
 #if LINUX_VERSION_CODE > KERNEL_VERSION(3,8,0)
 #define	__devinit
@@ -32,11 +23,11 @@
 #define	__devexit_p
 #endif
 
-static DEFINE_PCI_DEVICE_TABLE(pcidma_pci_tbl) = {
+static DEFINE_PCI_DEVICE_TABLE(ipnuma_pci_tbl) = {
 	{0x3776, 0x8010, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 0 },
 	{0,}
 };
-MODULE_DEVICE_TABLE(pci, pcidma_pci_tbl);
+MODULE_DEVICE_TABLE(pci, ipnuma_pci_tbl);
 
 static unsigned char *mmio0_ptr = 0L, *mmio1_ptr = 0L, *dma_virt_ptr = 0L;
 static dma_addr_t dma_phys_ptr = 0L;
@@ -46,156 +37,123 @@ static struct pci_dev *pcidev = NULL;
 static wait_queue_head_t write_q;
 static wait_queue_head_t read_q;
 
-/* receive and transmitte buffer */
-struct _pbuf_dma {
-	unsigned char	*rx_start_ptr;		/* buf start */
-	unsigned char 	*rx_end_ptr;		/* buf end */
-	unsigned char	*rx_write_ptr;		/* write ptr */
-	unsigned char	*rx_read_ptr;		/* read ptr */
-} static pbuf0={0,0,0,0,0}, pbuf1={0,0,0,0,0};
 
-
-static irqreturn_t pcidma_interrupt(int irq, struct pci_dev *pdev)
+static irqreturn_t ipnuma_interrupt(int irq, void *pdev)
 {
 	unsigned int frame_len;
 
-	frame_len = *(short *)(mmio0_ptr + RX_ADDR + 0x0c);
-#ifdef DEBUG
-	printk("%s\n", __func__);
-	printk( "frame_len=%d\n", frame_len);
-#endif
-
-	if (frame_len < 64)
-		frame_len = 64;
-
-	if (frame_len > 1518)
-		frame_len = 1518;
-
-	if ( (pbuf0.rx_write_ptr +  frame_len + 0x10) > pbuf0.rx_end_ptr ) {
-		memcpy( pbuf0.rx_start_ptr, pbuf0.rx_write_ptr, (pbuf0.rx_write_ptr - pbuf0.rx_start_ptr ));
-		pbuf0.rx_read_ptr -= (pbuf0.rx_write_ptr - pbuf0.rx_start_ptr );
-		if ( pbuf0.rx_read_ptr < pbuf0.rx_start_ptr )
-			pbuf0.rx_read_ptr = pbuf0.rx_start_ptr;
-		pbuf0.rx_write_ptr = pbuf0.rx_start_ptr;
+	// not my interrupt
+	if (1) {
+		return IRQ_NONE;
 	}
 
-	memcpy(pbuf0.rx_write_ptr+0x04, mmio0_ptr+RX_ADDR, 0x0c);
-	memcpy(pbuf0.rx_write_ptr+0x10, mmio0_ptr+RX_ADDR+0x10, frame_len);
-	
-	pbuf0.rx_write_ptr[0x00] = 0x55;			/* magic code 0x55d5 */
-	pbuf0.rx_write_ptr[0x01] = 0xd5;
-	*(short *)(pbuf0.rx_write_ptr + 2) = frame_len;
-
-	pbuf0.rx_write_ptr += (frame_len + 0x10);
-
-	*mmio0_ptr = 0x02;		/* IRQ clear and Request receiving PHY#0 */
+#ifdef DEBUG
+	printk("%s\n", __func__);
+#endif
 
 	wake_up_interruptible( &read_q );
 
 	return IRQ_HANDLED;
 }
 
-static int pcidma_open(struct inode *inode, struct file *filp)
+static int ipnuma_open(struct inode *inode, struct file *filp)
 {
 	printk("%s\n", __func__);
 
-	*mmio0_ptr = 0x02;		/* IRQ clear and Request receiving PHY#0 */
+//	*mmio0_ptr = 0x02;		/* IRQ clear and Request receiving PHY#0 */
 
 	return 0;
 }
 
-static ssize_t pcidma_read(struct file *filp, char __user *buf,
+static ssize_t ipnuma_read(struct file *filp, char __user *buf,
 			   size_t count, loff_t *ppos)
 {
-	int copy_len, available_read_len;
+	int copy_len;
 
 #ifdef DEBUG
 	printk("%s\n", __func__);
 #endif
 
+#if 0
 	if ( wait_event_interruptible( read_q, ( pbuf0.rx_read_ptr != pbuf0.rx_write_ptr ) ) )
 		return -ERESTARTSYS;
+#endif
 
-	available_read_len = (pbuf0.rx_write_ptr - pbuf0.rx_read_ptr);
+	copy_len = count;
 
-	if ( count > available_read_len )
-		copy_len = available_read_len;
-	else
-		copy_len = count;
-
-
+#if 0
 	if ( copy_to_user( buf, pbuf0.rx_read_ptr, copy_len ) ) {
 		printk( KERN_INFO "copy_to_user failed\n" );
 		return -EFAULT;
 	}
-
-	pbuf0.rx_read_ptr += copy_len;
+#endif
 
 	return copy_len;
 }
 
-static ssize_t pcidma_write(struct file *filp, const char __user *buf,
+static ssize_t ipnuma_write(struct file *filp, const char __user *buf,
 			    size_t count, loff_t *ppos)
 
 {
 	int copy_len;
-	if ( count > 256 )
-		copy_len = 256;
-	else
-		copy_len = count;
+
+	copy_len = count;
+
 #ifdef DEBUG
 	printk("%s\n", __func__);
 #endif
 
+#if 0
 	if ( copy_from_user( mmio0_ptr, buf, copy_len ) ) {
 		printk( KERN_INFO "copy_from_user failed\n" );
 		return -EFAULT;
 	}
+#endif
 
 	return copy_len;
 }
 
-static int pcidma_release(struct inode *inode, struct file *filp)
+static int ipnuma_release(struct inode *inode, struct file *filp)
 {
 	printk("%s\n", __func__);
 
-	*mmio0_ptr = 0x00;		/* IRQ clear and not Request receiving PHY#0 */
+//	*mmio0_ptr = 0x00;		/* IRQ clear and not Request receiving PHY#0 */
 
 	return 0;
 }
 
-static unsigned int pcidma_poll(struct file *filp, poll_table *wait)
+static unsigned int ipnuma_poll(struct file *filp, poll_table *wait)
 {
 	printk("%s\n", __func__);
 	return 0;
 }
 
 
-static int pcidma_ioctl(struct inode *inode, struct file *filp,
+static int ipnuma_ioctl(struct file *filp,
 			unsigned int cmd, unsigned long arg)
 {
 	printk("%s\n", __func__);
 	return  -ENOTTY;
 }
 
-static struct file_operations pcidma_fops = {
+static struct file_operations ipnuma_fops = {
 	.owner		= THIS_MODULE,
-	.read		= pcidma_read,
-	.write		= pcidma_write,
-	.poll		= pcidma_poll,
-	.compat_ioctl	= pcidma_ioctl,
-	.open		= pcidma_open,
-	.release	= pcidma_release,
+	.read		= ipnuma_read,
+	.write		= ipnuma_write,
+	.poll		= ipnuma_poll,
+	.unlocked_ioctl	= ipnuma_ioctl,
+	.open		= ipnuma_open,
+	.release	= ipnuma_release,
 };
 
-static struct miscdevice pcidma_dev = {
+static struct miscdevice ipnuma_dev = {
 	.minor = MISC_DYNAMIC_MINOR,
 	.name = DRV_NAME,
-	.fops = &pcidma_fops,
+	.fops = &ipnuma_fops,
 };
 
 
-static int __devinit pcidma_init_one (struct pci_dev *pdev,
+static int __devinit ipnuma_init_one (struct pci_dev *pdev,
 				       const struct pci_device_id *ent)
 {
 	int rc;
@@ -247,7 +205,7 @@ static int __devinit pcidma_init_one (struct pci_dev *pdev,
 		goto err_out;
 	}
 
-	dma_virt_ptr = dma_alloc_coherent( &pdev->dev, 1024*1024, &dma_phys_ptr, GFP_KERNEL);
+	dma_virt_ptr = dma_alloc_coherent( &pdev->dev, DMA_BUF_SIZE, &dma_phys_ptr, GFP_KERNEL);
 	if (!dma_virt_ptr) {
 		printk(KERN_ERR "cannot dma_alloc_coherent\n");
 		goto err_out;
@@ -255,12 +213,13 @@ static int __devinit pcidma_init_one (struct pci_dev *pdev,
 	printk( KERN_INFO "dma_virt_ptr  : %X\n", dma_virt_ptr );
 	printk( KERN_INFO "dma_phys_ptr  : %X\n", dma_phys_ptr );
 
-	if (request_irq(pdev->irq, pcidma_interrupt, IRQF_SHARED, DRV_NAME, pdev)) {
+	if (request_irq(pdev->irq, ipnuma_interrupt, IRQF_SHARED, DRV_NAME, pdev)) {
 		printk(KERN_ERR "cannot request_irq\n");
 	}
 	
-	/* reset board */
 	pcidev = pdev;
+
+	/* reset board */
 	*mmio0_ptr = 0x02;	/* Request receiving PHY#1 */
 
 	return 0;
@@ -272,7 +231,7 @@ err_out:
 }
 
 
-static void __devexit pcidma_remove_one (struct pci_dev *pdev)
+static void __devexit ipnuma_remove_one (struct pci_dev *pdev)
 {
 	disable_irq(pdev->irq);
 	free_irq(pdev->irq, pdev);
@@ -291,59 +250,51 @@ static void __devexit pcidma_remove_one (struct pci_dev *pdev)
 }
 
 
-static struct pci_driver pcidma_pci_driver = {
+static struct pci_driver ipnuma_pci_driver = {
 	.name		= DRV_NAME,
-	.id_table	= pcidma_pci_tbl,
-	.probe		= pcidma_init_one,
-	.remove		= __devexit_p(pcidma_remove_one),
+	.id_table	= ipnuma_pci_tbl,
+	.probe		= ipnuma_init_one,
+	.remove		= __devexit_p(ipnuma_remove_one),
 #ifdef CONFIG_PM
-//	.suspend	= pcidma_suspend,
-//	.resume		= pcidma_resume,
+//	.suspend	= ipnuma_suspend,
+//	.resume		= ipnuma_resume,
 #endif /* CONFIG_PM */
 };
 
 
-static int __init pcidma_init(void)
+static int __init ipnuma_init(void)
 {
 	int ret;
 
 #ifdef MODULE
-	pr_info(pcidma_DRIVER_NAME "\n");
+	pr_info(ipnuma_DRIVER_NAME "\n");
 #endif
 
-	ret = misc_register(&pcidma_dev);
+	ret = misc_register(&ipnuma_dev);
 	if (ret) {
 		printk("fail to misc_register (MISC_DYNAMIC_MINOR)\n");
 		return ret;
 	}
 
-	if ( ( pbuf0.rx_start_ptr = kmalloc( PACKET_BUF_MAX, GFP_KERNEL) ) == 0 ) {
-		printk("fail to kmalloc\n");
-		return -1;
-	}
-	pbuf0.rx_end_ptr = (pbuf0.rx_start_ptr + PACKET_BUF_MAX - 1);
-	pbuf0.rx_write_ptr = pbuf0.rx_start_ptr;
-	pbuf0.rx_read_ptr  = pbuf0.rx_start_ptr;
-
 	init_waitqueue_head( &write_q );
 	init_waitqueue_head( &read_q );
 	
 	printk("%s\n", __func__);
-	return pci_register_driver(&pcidma_pci_driver);
+	return pci_register_driver(&ipnuma_pci_driver);
 }
 
-static void __exit pcidma_cleanup(void)
+static void __exit ipnuma_cleanup(void)
 {
 	printk("%s\n", __func__);
-	misc_deregister(&pcidma_dev);
-	pci_unregister_driver(&pcidma_pci_driver);
-	if ( pbuf0.rx_start_ptr )
-		kfree( pbuf0.rx_start_ptr );
+	misc_deregister(&ipnuma_dev);
+	pci_unregister_driver(&ipnuma_pci_driver);
+
 	if ( dma_virt_ptr )
-		dma_free_coherent(&pcidma_dev, 1024*1024, dma_virt_ptr, dma_phys_ptr);
+		dma_free_coherent(&pcidev->dev, DMA_BUF_SIZE, dma_virt_ptr, dma_phys_ptr);
+
 }
 
 MODULE_LICENSE("GPL");
-module_init(pcidma_init);
-module_exit(pcidma_cleanup);
+module_init(ipnuma_init);
+module_exit(ipnuma_cleanup);
 
