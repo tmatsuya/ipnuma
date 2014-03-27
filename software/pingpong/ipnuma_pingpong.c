5f4ec2969d9b151b8561f8c7571e8fae3f25c196
Takeshi MATSUYA <macchan@sfc.wide.ad.jp>
2014-03-27 16:25:36 +0900

    ipnuma_pingpong.c: set mac and ipv4 address

--- software/pingpong/ipnuma_pingpong.c
+++ software/pingpong/ipnuma_pingpong.c
@@ -23,30 +23,57 @@ TimeWatcher tw;
 int numa_fd = -1;
 unsigned long pa, mem0p, baraddr;
 unsigned char if_ipv4[4], if_mac[6];
+unsigned char dest_ipv4[4], dest_mac[6];
+
+unsigned char srv_ipv4[4] = {192,168,1,1};
+unsigned char srv_mac[6]  = {0x00,0x37,0x76,0x10,0x00,0x01};
+unsigned char cli_ipv4[4] = {192,168,1,2};
+unsigned char cli_mac[6]  = {0x00,0x37,0x76,0x10,0x00,0x02};
 
 
-int init_numa()
+int init_numa(int server_flag)
 {
 	if (numa_fd <0)
 		if ((numa_fd=open(NUMA_DEVICE,O_RDONLY)) <0) {
 			fprintf(stderr,"cannot open %s\n",NUMA_DEVICE);
 			return 1;
 		}
+	if (server_flag) {
+		memcpy(if_ipv4, srv_ipv4, 4);
+		memcpy(if_mac,  srv_mac, 6);
+		memcpy(dest_ipv4, cli_ipv4, 4);
+		memcpy(dest_mac, cli_mac, 6);
+	} else {
+		memcpy(if_ipv4, cli_ipv4, 4);
+		memcpy(if_mac,  cli_mac, 6);
+		memcpy(dest_ipv4, srv_ipv4, 4);
+		memcpy(dest_mac, srv_mac, 6);
+	}
 	if ( ioctl( numa_fd, IPNUMA_IOCTL_GETPADDR, &pa) < 0 ) {
 		fprintf(stderr,"cannot IOCTL\n");
 		return 1;
 	}
 	printf("physical address=%012lX\n", pa);
-	if ( ioctl( numa_fd, IPNUMA_IOCTL_GETIFV4ADDR, if_ipv4) < 0 ) {
+	if ( ioctl( numa_fd, IPNUMA_IOCTL_SETIFV4ADDR, if_ipv4) < 0 ) {
 		fprintf(stderr,"cannot IOCTL\n");
 		return 1;
 	}
 	printf("if_ipv4=%d.%d.%d.%d\n", if_ipv4[0], if_ipv4[1], if_ipv4[2], if_ipv4[3]);
-	if ( ioctl( numa_fd, IPNUMA_IOCTL_GETIFMACADDR, if_mac) < 0 ) {
+	if ( ioctl( numa_fd, IPNUMA_IOCTL_SETIFMACADDR, if_mac) < 0 ) {
 		fprintf(stderr,"cannot IOCTL\n");
 		return 1;
 	}
 	printf("if_mac=%02x:%02x:%02x:%02x:%02x:%02x\n", if_mac[0], if_mac[1], if_mac[2], if_mac[3], if_mac[4], if_mac[5] ); 
+	if ( ioctl( numa_fd, IPNUMA_IOCTL_SETDESTV4ADDR, dest_ipv4) < 0 ) {
+		fprintf(stderr,"cannot IOCTL\n");
+		return 1;
+	}
+	printf("dest_ipv4=%d.%d.%d.%d\n", dest_ipv4[0], dest_ipv4[1], dest_ipv4[2], dest_ipv4[3]);
+	if ( ioctl( numa_fd, IPNUMA_IOCTL_SETDESTMACADDR, dest_mac) < 0 ) {
+		fprintf(stderr,"cannot IOCTL\n");
+		return 1;
+	}
+	printf("dest_mac=%02x:%02x:%02x:%02x:%02x:%02x\n", dest_mac[0], dest_mac[1], dest_mac[2], dest_mac[3], dest_mac[4], dest_mac[5] ); 
 	if ( ioctl( numa_fd, IPNUMA_IOCTL_GETMEM0PADDR, &mem0p) < 0 ) {
 		fprintf(stderr,"cannot IOCTL\n");
 		return 1;
@@ -75,7 +102,10 @@ int main(int argc,char **argv)
 	}
 
 	pa = (unsigned long)&rdata;
-	init_numa();
+	if ( !strcmp( argv[1], "s") )
+		init_numa(1);
+	else
+		init_numa(0);
 
 	st = baraddr;
 	poff=st % 4096;
