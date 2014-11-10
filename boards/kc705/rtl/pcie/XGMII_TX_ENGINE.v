de6a37c20d327d5cc10e9675d25f3571cfd1540f
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2014-11-10 14:16:44 +0900

    XGMII_TX_ENGINE.v: packet size is 68

--- boards/kc705/rtl/pcie/XGMII_TX_ENGINE.v
+++ boards/kc705/rtl/pcie/XGMII_TX_ENGINE.v
@@ -66,9 +66,7 @@ reg crc_init = 1'b0;
 reg crc_rewrite = 1'b0;
 assign crc_data_en = ~crc_init;
 wire [31:0] crc64_out, crc64_outrev;
-wire [31:0] crc32_out, crc32_outrev;
 assign crc64_outrev = ~{ crc64_out[24],crc64_out[25],crc64_out[26],crc64_out[27],crc64_out[28],crc64_out[29],crc64_out[30],crc64_out[31], crc64_out[16],crc64_out[17],crc64_out[18],crc64_out[19],crc64_out[20],crc64_out[21],crc64_out[22],crc64_out[23], crc64_out[ 8],crc64_out[ 9],crc64_out[10],crc64_out[11],crc64_out[12],crc64_out[13],crc64_out[14],crc64_out[15], crc64_out[ 0],crc64_out[ 1],crc64_out[ 2],crc64_out[ 3],crc64_out[ 4],crc64_out[ 5],crc64_out[ 6],crc64_out[ 7] };
-assign crc32_outrev = ~{ crc32_out[24],crc32_out[25],crc32_out[26],crc32_out[27],crc32_out[28],crc32_out[29],crc32_out[30],crc32_out[31], crc32_out[16],crc32_out[17],crc32_out[18],crc32_out[19],crc32_out[20],crc32_out[21],crc32_out[22],crc32_out[23], crc32_out[ 8],crc32_out[ 9],crc32_out[10],crc32_out[11],crc32_out[12],crc32_out[13],crc32_out[14],crc32_out[15], crc32_out[ 0],crc32_out[ 1],crc32_out[ 2],crc32_out[ 3],crc32_out[ 4],crc32_out[ 5],crc32_out[ 6],crc32_out[ 7] };
 
 crc32_d64 crc32_d64_inst (
 	.rst(crc_init),
@@ -86,17 +84,6 @@ txd[60],txd[61],txd[62],txd[63]
 	.crc_out(crc64_out)	// 32bit
 );
 
-crc32_d32 crc32_d32_inst (
-	.data_in({
-txd[00],txd[01],txd[02],txd[03],txd[04],txd[05],txd[06],txd[07],txd[08],txd[09],
-txd[10],txd[11],txd[12],txd[13],txd[14],txd[15],txd[16],txd[17],txd[18],txd[19],
-txd[20],txd[21],txd[22],txd[23],txd[24],txd[25],txd[26],txd[27],txd[28],txd[29],
-txd[30],txd[31]
-}),
-	.crc_in(crc64_out),
-	.crc_out(crc32_out)
-);
-
 //-----------------------------------
 // scenario parameter
 //-----------------------------------
@@ -108,12 +95,13 @@ reg [23:0] full_ipv4;
 reg [23:0] ip_sum;
 
 reg [31:0] gap_count;
-parameter TX_V4_SEND     = 2'b00;  // IPv4 Payload
-parameter TX_GAP	 = 2'b01;  // Inter Frame Gap
+parameter TX_IDLE        = 2'b00;  // IDLE
+parameter TX_V4_SEND     = 2'b01;  // IPv4 Payload
+parameter TX_GAP	 = 2'b10;  // Inter Frame Gap
 reg [1:0] tx_state;
 
 wire [15:0] tx0_frame_len;
-assign tx0_frame_len = 16'd64;
+assign tx0_frame_len = 16'd68;
 wire [15:0] tx0_inter_frame_gap;
 assign tx0_inter_frame_gap = 16'd1;
 
@@ -134,13 +122,10 @@ always @(posedge xgmii_clk) begin
 		full_ipv4 <= 24'h0;
 		tx_state <= TX_V4_SEND;
 	end else begin
-		crc_rewrite <= 1'b0;
-		txc2 <= txc;
-		if (crc_rewrite == 1'b0)
-			txd2 <= txd;
-		else
-			txd2 <= {crc32_outrev[7:0], crc32_outrev[15:8], crc32_outrev[23:16], crc32_outrev[31:24], txd[31:0]};
 		case (tx_state)
+		TX_IDLE: begin
+			tx_state <= TX_V4_SEND;
+		end
 		TX_V4_SEND: begin
 			tx_counter <= tx_counter + 32'h8;
 			case (tx_counter[15:0] )
@@ -159,17 +144,17 @@ always @(posedge xgmii_clk) begin
 				16'h20: {txc, txd} <= {8'h00, ipv4_dstip[23:16], ipv4_dstip[31:24], if_v4addr[7:0], if_v4addr[15:8], if_v4addr[23:16], if_v4addr[31:24], ip_sum[7:0], ip_sum[15:8]};
 				16'h28: {txc, txd} <= {8'h00, tx0_udp_len[7:0], 4'h0, tx0_udp_len[11:8], 32'h09_00_09_00, ipv4_dstip[7:0], ipv4_dstip[15:8]};
 				16'h30: {txc, txd} <= {8'h00, 16'h00, magic_code[7:0], magic_code[15:8], magic_code[23:16], magic_code[31:24], 16'h00_00};
-				16'h38: {txc, txd} <= {8'h00, 64'h00_00_00_00_00_00_00_00};
+				16'h38: {txc, txd} <= {8'h00, 64'h07_06_05_04_03_02_01_00};
 				16'h40: begin
-					{txc, txd} <= {8'h00, 64'he5_e5_e5_e5_00_00_00_00};
+					{txc, txd} <= {8'h00, 64'h0f_0e_0d_0c_0b_0a_09_08};
 					crc_rewrite <= 1'b1;
 				end
 				default: begin
-					{txc, txd} <= {8'hff, 64'h07_07_07_07_07_07_07_fd};
+					{txc, txd} <= {8'hff, 32'h07_07_07_fd, crc64_outrev[7:0], crc64_outrev[15:8], crc64_outrev[23:16], crc64_outrev[31:24]};
 					tx_counter <= 32'h0;
 					if (tx0_inter_frame_gap == 32'd0) begin
 						full_ipv4 <= full_ipv4 + 24'h1;
-						tx_state <= TX_V4_SEND;
+						tx_state <= TX_IDLE;
 					end else begin
 						gap_count <= tx0_inter_frame_gap - 32'd1;
 						tx_state <= TX_GAP;
@@ -182,13 +167,13 @@ always @(posedge xgmii_clk) begin
 			gap_count <= gap_count - 32'd1;
 			if (gap_count == 32'd0) begin
 				full_ipv4 <= full_ipv4 + 24'h1;
-				tx_state <= TX_V4_SEND;
+				tx_state <= TX_IDLE;
 			end
 		end
 		endcase
 	end
 end
 
-assign xgmii_txd = {txc2, txd2};
+assign xgmii_txd = {txc, txd};
 
 endmodule
