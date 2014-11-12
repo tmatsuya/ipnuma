065cdcdb9dda4e9a723e68f6e681f253ec6ddfc2
Takeshi MATSUYA <tmatsuya@matcchan-no-MacBook-Air.local>
2014-11-13 00:19:50 +0900

    XGMII_RX_ENGINE: fixed TLP header bugs

--- boards/kc705/rtl/pcie/XGMII_RX_ENGINE.v
+++ boards/kc705/rtl/pcie/XGMII_RX_ENGINE.v
@@ -3,26 +3,28 @@
 `else
 `include "../setup.v"
 `endif
-module XGMII_RX_ENGINE (
-	input sys_rst,
-	input clk,
+module XGMII_RX_ENGINE # (
+	parameter Gap = 4'h5
+) (
+	input wire sys_rst,
+	input wire clk,
 	// XGMII
-	input xgmii_clk,
-	input [7:0] xgmii_rxc,
-	input [63:0] xgmii_rxd,
+	input wire xgmii_clk,
+	input wire [7:0] xgmii_rxc,
+	input wire [63:0] xgmii_rxd,
 	// PCIe user registers
-	input [31:0] if_v4addr,
-	input [47:0] if_macaddr,
-	input [31:0] dest_v4addr,
-	input [47:0] dest_macaddr,
+	input wire [31:0] if_v4addr,
+	input wire [47:0] if_macaddr,
+	input wire [31:0] dest_v4addr,
+	input wire [47:0] dest_macaddr,
 	// XGMII-RX FIFO
-	output [71:0] din,
+	output reg [71:0] din,
 	input full,
 	output reg wr_en,
 
-	output [7:0] xgmii_pktcount,
+	output reg [7:0] xgmii_pktcount,
 
-	output [7:0] led
+	output wire [7:0] led
 );
 
 reg [7:0] led_r = 8'h00;
@@ -36,14 +38,15 @@ reg [15:0] rx_type;	 // frame type
 reg [47:0] rx_src_mac;
 reg [7:0] rx_protocol;
 reg [15:0] rx_dport;
-reg bit64 = 1'b0;
-reg [9:0] length = 10'h0;
+reg [9:0] dwlen = 10'h0;
 reg tlp_frame_end = 1'b0;
+reg [3:0] gap = 4'h0;
 
 parameter RX_IDLE  = 3'b000;
 parameter RX_HEAD  = 3'b001;
 parameter RX_TLP1  = 3'b010;
 parameter RX_TLP2  = 3'b011;
+parameter RX_GAP   = 3'b100;
 reg [2:0] rx_state = RX_IDLE;
 
 always @(posedge xgmii_clk) begin
@@ -54,18 +57,19 @@ always @(posedge xgmii_clk) begin
 		rx_src_mac <= 48'h0;
 		rx_protocol <= 8'h0;
 		rx_dport <= 16'h00;
-		bit64 <= 1'b0;
-		length <= 10'h0;
+		dwlen <= 10'h0;
 		wr_en <= 1'b0;
 		led_r <= 8'h00;
 		tlp_frame_end <= 1'b0;
 		rx_state <= RX_IDLE;
 	end else begin
 		tlp_frame_end <= 1'b0;
+		din[63:0] <= xgmii_rxd;
 		wr_en <= 1'b0;
 		case (rx_state)
 		RX_IDLE: begin
 			rx_count <= 16'h0;
+			gap <= Gap;
 			if (xgmii_rxc[0] == 1'b1 && xgmii_rxd[7:0] == 8'hfb)
 				rx_state <= RX_HEAD;
 		end
@@ -92,28 +96,51 @@ always @(posedge xgmii_clk) begin
 			16'h20: begin
 				rx_dport[15:8]    <= xgmii_rxd[39:32];
 				rx_dport[ 7:0]    <= xgmii_rxd[47:40];
-				tlp_frame_end <= 1'b1;
 			end
 			16'h28: begin
-				tlp_frame_end <= 1'b1;
 				rx_magic[31:24] <= xgmii_rxd[23:16];
 				rx_magic[23:16] <= xgmii_rxd[31:24];
 				rx_magic[15:8]  <= xgmii_rxd[39:32];
 				rx_magic[7:0]   <= xgmii_rxd[47:40];
 				if (rx_type == 16'h0800 && rx_protocol == 8'h11 && rx_dport == 16'd3422 && {xgmii_rxd[23:16], xgmii_rxd[31:24], xgmii_rxd[39:32], xgmii_rxd[47:40]} == `MAGIC_CODE) begin
+					led_r <= led_r + 8'd1; // xgmii_rxd[7:0];
 					rx_state <= RX_TLP1;
 				end
 			end
 			endcase
 		end
 		RX_TLP1: begin
-			bit64 <= xgmii_rxd[29];
-			length <= xgmii_rxd[9:0];
-			led_r <= xgmii_rxd[7:0];
-			rx_state <= RX_TLP2;
+			if (xgmii_rxc[7:0] != 8'h00)
+				rx_state <= RX_GAP;
+			else begin
+				din[71:64] <= 8'b0000_1101; // Start TLP
+				// bit64:xgmii_rxd[29]; length:xgmii_rxd[9:0]
+				dwlen <= xgmii_rxd[9:0] + 10'd1 + {9'd0, xgmii_rxd[29]}; // tlp DWlength
+				wr_en <= 1'b1;
+				rx_state <= RX_TLP2;
+			end
 		end
 		RX_TLP2: begin
-			rx_state <= RX_IDLE;
+			wr_en <= 1'b1;
+			dwlen <= dwlen - 10'd2;
+			if (dwlen == 10'd0) begin
+				din[71:64] <= 8'b0000_1110; // End TLP (2DW)
+				tlp_frame_end <= 1'b1;
+				rx_state <= RX_TLP1;
+			end else if (dwlen == 10'd1023) begin
+				din[71:64] <= 8'b0000_0110; // End TLP (1DW)
+				tlp_frame_end <= 1'b1;
+				rx_state <= RX_TLP1;
+			end else begin
+				din[71:64] <= 8'b0000_1100; // TLP (2DW)
+			end
+		end
+		RX_GAP: begin
+			gap <= gap - 4'h1;
+			if (gap == 4'h0)
+				rx_state <= RX_IDLE;
+			din <= 72'h00;
+			wr_en <= 1'b1;
 		end
 		endcase
 	end
@@ -123,19 +150,17 @@ end
 // count XGMII frame
 //-----------------------------------
 reg [3:0] prev_xgmii_end;
-reg [7:0] xgmii_packet_count;
 always @(posedge clk) begin
 	if (sys_rst) begin
-		xgmii_packet_count <= 8'h0;
+		xgmii_pktcount <= 8'h0;
 		prev_xgmii_end <= 4'b0000;
 	end else begin
 		prev_xgmii_end <= {tlp_frame_end, prev_xgmii_end[3:1]};
 		if (prev_xgmii_end == 4'b0001)
-			xgmii_packet_count <= xgmii_packet_count + 8'd1;
+			xgmii_pktcount <= xgmii_pktcount + 8'd1;
 	end
 end
 
 assign led = led_r;
-assign xgmii_pktcount = xgmii_packet_count; 
 
 endmodule
