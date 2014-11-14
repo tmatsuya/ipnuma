b6b44db400c897c86f2f64a2c75e625bdd586360
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2014-11-14 20:08:49 +0900

    loop.sh: there are 4 mode testing

--- software/kc705_test/loop.sh
+++ software/kc705_test/loop.sh
@@ -5,23 +5,23 @@ do
 sudo ifconfig eth5 192.168.1.1 up
 sudo arp -s 192.168.1.2 00:37:76:00:00:01
 mode=0
-./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x407000f4
+./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x307000f4
 sleep 1
-./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x400700f4
+./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x300700f4
 sleep 1
 mode=1
-./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x407000f4
+./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x317000f4
 sleep 1
-./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x400700f4
+./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x310700f4
 sleep 1
 mode=2
-./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x407000f4
+./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x327000f4
 sleep 1
-./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x400700f4
+./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x320700f4
 sleep 1
 mode=3
-./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x407000f4
+./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x337000f4
 sleep 1
-./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x400700f4
+./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x330700f4
 sleep 1
 done
