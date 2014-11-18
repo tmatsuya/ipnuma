27f1929331ec8253f59d3e1bf7975061331b224d
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2014-11-18 14:05:15 +0900

    10G ether port assigned eth1 for testing

--- software/kc705_test/loop.sh
+++ software/kc705_test/loop.sh
@@ -2,7 +2,7 @@
 mode=0
 while true
 do
-sudo ifconfig eth5 192.168.1.1 up
+sudo ifconfig eth1 192.168.1.1 up
 sudo arp -s 192.168.1.2 00:37:76:00:00:01
 mode=0
 ./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x307000f4
--- software/kc705_test/test.sh
+++ software/kc705_test/test.sh
@@ -1,5 +1,5 @@
-sudo ifconfig eth5 down
-sudo ifconfig eth5 192.168.1.1 up
+sudo ifconfig eth1 down
+sudo ifconfig eth1 192.168.1.1 up
 sudo arp -s 192.168.1.2 00:37:76:00:00:01
 exit
 done
@@ -8,7 +8,7 @@ done
 mode=0
 while true
 do
-sudo ifconfig eth5 192.168.1.1 up
+sudo ifconfig eth1 192.168.1.1 up
 sudo arp -s 192.168.1.2 00:37:76:00:00:01
 ./ipnuma_client 192.168.1.2 $mode 0xd0000000 0x407000f4
 sleep 1
