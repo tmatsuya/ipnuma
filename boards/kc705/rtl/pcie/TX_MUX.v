16e23b51b770a95e0182868b61efdc4e5e7c2120
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2015-01-02 15:27:06 +0900

    TX_MUX.v: update

--- boards/kc705/rtl/pcie/TX_MUX.v
+++ boards/kc705/rtl/pcie/TX_MUX.v
@@ -1,3 +1,4 @@
+`default_nettype none
 `timescale 1ps/1ps
 
 module TX_MUX # (
@@ -7,33 +8,33 @@ module TX_MUX # (
 	// TSTRB width
 	parameter KEEP_WIDTH = C_DATA_WIDTH / 8
 )(
-	input clk,
-	input sys_rst,
+	input wire clk,
+	input wire sys_rst,
 	// AXIS Output
-	input s_axis_tx_tready,
-	output [C_DATA_WIDTH-1:0] s_axis_tx_tdata,
-	output [KEEP_WIDTH-1:0] s_axis_tx_tkeep,
-	output s_axis_tx_tlast,
-	output s_axis_tx_tvalid,
-	output tx_src_dsc,
+	input wire s_axis_tx_tready,
+	output wire [C_DATA_WIDTH-1:0] s_axis_tx_tdata,
+	output wire [KEEP_WIDTH-1:0] s_axis_tx_tkeep,
+	output wire s_axis_tx_tlast,
+	output wire s_axis_tx_tvalid,
+	output wire tx_src_dsc,
 	// AXIS Input 1
-	input s_axis_tx1_req,
+	input wire s_axis_tx1_req,
 	output reg s_axis_tx1_ack = 1'b0,
-	output s_axis_tx1_tready,
-	input [C_DATA_WIDTH-1:0] s_axis_tx1_tdata,
-	input [KEEP_WIDTH-1:0] s_axis_tx1_tkeep,
-	input s_axis_tx1_tlast,
-	input s_axis_tx1_tvalid,
-	input tx1_src_dsc,
+	output wire s_axis_tx1_tready,
+	input wire [C_DATA_WIDTH-1:0] s_axis_tx1_tdata,
+	input wire [KEEP_WIDTH-1:0] s_axis_tx1_tkeep,
+	input wire s_axis_tx1_tlast,
+	input wire s_axis_tx1_tvalid,
+	input wire tx1_src_dsc,
 	// AXIS Input 2
-	input s_axis_tx2_req,
+	input wire s_axis_tx2_req,
 	output reg s_axis_tx2_ack = 1'b0,
-	output s_axis_tx2_tready,
-	input [C_DATA_WIDTH-1:0] s_axis_tx2_tdata,
-	input [KEEP_WIDTH-1:0] s_axis_tx2_tkeep,
-	input s_axis_tx2_tlast,
-	input s_axis_tx2_tvalid,
-	input tx2_src_dsc
+	output wire s_axis_tx2_tready,
+	input wire [C_DATA_WIDTH-1:0] s_axis_tx2_tdata,
+	input wire [KEEP_WIDTH-1:0] s_axis_tx2_tkeep,
+	input wire s_axis_tx2_tlast,
+	input wire s_axis_tx2_tvalid,
+	input wire tx2_src_dsc
 );
 
 always @(posedge clk) begin
@@ -53,12 +54,18 @@ always @(posedge clk) begin
 			end
 		end
 		2'b01: begin
-			if (~s_axis_tx1_req)
+			if (~s_axis_tx1_req) begin
 				s_axis_tx1_ack <= 1'b0;
+				if (s_axis_tx2_req)
+					s_axis_tx2_ack <= 1'b1;
+			end
 		end
 		2'b10: begin
-			if (~s_axis_tx2_req)
+			if (~s_axis_tx2_req) begin
 				s_axis_tx2_ack <= 1'b0;
+				if (s_axis_tx1_req)
+					s_axis_tx1_ack <= 1'b1;
+			end
 		end
 		2'b11: begin
 		end
@@ -66,12 +73,14 @@ always @(posedge clk) begin
 	end
 end
 
-assign s_axis_tx1_tready= s_axis_tx_tready;
-assign s_axis_tx2_tready= s_axis_tx_tready;
+assign s_axis_tx1_tready= s_axis_tx_tready & s_axis_tx1_ack;
+assign s_axis_tx2_tready= s_axis_tx_tready & s_axis_tx2_ack;
 assign s_axis_tx_tdata  = s_axis_tx2_ack ? s_axis_tx2_tdata:  s_axis_tx1_tdata;
 assign s_axis_tx_tkeep  = s_axis_tx2_ack ? s_axis_tx2_tkeep:  s_axis_tx1_tkeep;
 assign s_axis_tx_tlast  = s_axis_tx2_ack ? s_axis_tx2_tlast:  s_axis_tx1_tlast;
 assign s_axis_tx_tvalid = s_axis_tx2_ack ? s_axis_tx2_tvalid: s_axis_tx1_tvalid;
 assign tx_src_dsc       = s_axis_tx2_ack ? tx2_src_dsc:       tx1_src_dsc;
 
+
 endmodule // TX_MUX
+`default_nettype wire
